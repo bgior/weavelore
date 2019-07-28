85d49c053d78784d8c7a3b4a28136301ee1083a1
Bruno Giorello <bruno.giorello@gmail.com>
2019-07-27 23:48:41 -0300

    Bump app version on worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,7 +9,7 @@ change the cache suffix. This was configured in vue.config.js. */
 
 // Add a suffix so that the cache of a new worker doesn't overwrite an old one's.
 // This matters because the new worker won't immediately become active.
-workbox.core.setCacheNameDetails({ prefix: "weavelore", suffix: '0.23.1' });
+workbox.core.setCacheNameDetails({ prefix: "weavelore", suffix: '0.24' });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
