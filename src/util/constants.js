58c438e0133d2a10b3ed8ad69e026257cc8361b6
Bruno Giorello <bruno.giorello@gmail.com>
2019-08-31 00:38:28 -0300

    Bump app version

--- src/util/constants.js
+++ src/util/constants.js
@@ -2,7 +2,7 @@
 
 // This file defines some application-wide constants.
 export default {
-  appVersion: '0.26.0',
+  appVersion: '0.27.0',
   srdVersion: 14,
   levels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   schools: ['abjuration', 'conjuration', 'divination', 'enchantment', 'evocation', 'illusion', 'necromancy', 'transmutation'],
