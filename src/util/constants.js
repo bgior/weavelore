67cc6a083e2d81976c50bf3a08bba6419dcc0fd5
Bruno Giorello <bruno.giorello@gmail.com>
2020-05-17 02:10:09 -0300

    Fix SRD version and bump app version

--- src/util/constants.js
+++ src/util/constants.js
@@ -2,8 +2,8 @@
 
 // This file defines some application-wide constants.
 export default {
-  appVersion: '1.0.0',
-  srdVersion: 14,
+  appVersion: '1.0.1',
+  srdVersion: 15,
   levels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   schools: ['abjuration', 'conjuration', 'divination', 'enchantment', 'evocation', 'illusion', 'necromancy', 'transmutation'],
   classes: ['bard', 'cleric', 'druid', 'paladin', 'ranger', 'sorcerer', 'warlock', 'wizard'],
