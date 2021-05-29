fa7214754cdd1a08a65ece8ee9aea6a6bb85d0d1
Bruno Giorello <bruno.giorello@gmail.com>
2021-05-28 23:53:59 -0300

    Bump version to 1.0.2

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,7 +10,7 @@ change the cache suffix. This was configured in vue.config.js. */
 /* eslint no-undef: 0 */
 // Add a suffix so that the cache of a new worker doesn't overwrite an old one's.
 // This matters because the new worker won't immediately become active.
-workbox.core.setCacheNameDetails({ prefix: "weavelore", suffix: '1.0.1' });
+workbox.core.setCacheNameDetails({ prefix: "weavelore", suffix: '1.0.2' });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 
--- src/util/constants.js
+++ src/util/constants.js
@@ -2,7 +2,7 @@
 
 // This file defines some application-wide constants.
 export default {
-  appVersion: '1.0.1',
+  appVersion: '1.0.2',
   srdVersion: 15,
   levels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
   schools: ['abjuration', 'conjuration', 'divination', 'enchantment', 'evocation', 'illusion', 'necromancy', 'transmutation'],
