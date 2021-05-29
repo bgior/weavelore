cd6ebaec2643013166fa99d7a988e00cb000f7f5
Bruno Giorello <bruno.giorello@gmail.com>
2021-05-28 23:41:51 -0300

    Fix OGL page text

--- src/components/general/OGLPage.vue
+++ src/components/general/OGLPage.vue
@@ -4,8 +4,7 @@
   <div class="row text-left justify-content-center">
     <div class="col-12 col-md-10 col-xl-8">
       <h1 class="my-4">About OGL<img src="@/assets/images/icons/menu/ogl.png" alt=""/></h1>
-      <p>The default application data is Open Game Content from the <b>SRD</b> (System Reference Document) 5.1, released under the terms of the <b>OGL</b> (Open Game License), is used by the application.</p>
-      <p>A copy of the license text is included below in accordance to the terms:</p>
+      <p>The default application data is Open Game Content from the <b>SRD</b> (System Reference Document) 5.1, released under the terms of the <b>OGL</b> (Open Game License). A copy of the license text is included below in accordance to the terms:</p>
       <div v-html="licenseText" class="license-text my-4">
       </div>
       <p>The SRD can be found <a href="https://dnd.wizards.com/articles/features/systems-reference-document-srd">here</a>.</p>
